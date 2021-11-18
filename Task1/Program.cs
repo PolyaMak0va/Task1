49a5518442d5f91f0493d8a67dfa094f3f4164ca
Ксения Финаева <polya.makova@outlook.com>
2021-11-18 17:19:23 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (n > 0)
+            {
+                for (int i = 1; i <= (2 * n - 1); i += 2)
+                {
+                    Console.WriteLine("Текущее значение суммы: {0}", sum += i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите число больше 0.");
+            }
+            Console.ReadKey();
+        }
+    }
+}

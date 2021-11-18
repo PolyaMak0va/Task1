528f4254194d1174ff1e266a0c4070beedae7466
Ксения Финаева <polya.makova@outlook.com>
2021-11-18 17:27:11 +0300

    Убраны условные операторы, т.к. неправильно прочла задание (без других операторов, только FOR)

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -13,16 +13,9 @@ namespace Task1
             Console.WriteLine("Введите N");
             int n = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
-            if (n > 0)
+            for (int i = 1; i <= (2 * n - 1); i += 2)
             {
-                for (int i = 1; i <= (2 * n - 1); i += 2)
-                {
-                    Console.WriteLine("Текущее значение суммы: {0}", sum += i);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Ошибка! Введите число больше 0.");
+                Console.WriteLine("Текущее значение суммы: {0}", sum += i);
             }
             Console.ReadKey();
         }

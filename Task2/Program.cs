5c24825f85e9103207c6b251929b3892dd5d9353
Ксения Финаева <polya.makova@outlook.com>
2021-11-26 22:52:19 +0300

    Исправлено 26.11.21

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,6 +10,17 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            int i = Convert.ToInt32(Console.ReadLine());
+            double Sum2 = Sum * 2;
+            double P = 4;
+            int year = 0;
+            do
+            {
+                Sum += Sum * P / 100;
+                year++;
+            } while (i=0);
+            Console.WriteLine(year);
+            Console.ReadKey();
         }
     }
 }
